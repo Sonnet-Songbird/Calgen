4948790b41bdaba32563a50654f0ce8e5964e87a
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-02-02 16:02:28 +0900

    FEAT: Add Factory method for Class Operation, Class.FCC Operations
    this handle construction by String with Translator

--- src/main/java/org/songbird/calgen/domain/Calculator/Operation.java
+++ src/main/java/org/songbird/calgen/domain/Calculator/Operation.java
@@ -1,13 +1,41 @@
 package org.songbird.calgen.domain.Calculator;
+
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Optional;
 
 class Operation {
     private final Operator operator;
     private Operand operand;
 
-    public Operation(Operator operator, Operand operand) {
+
+    private Operation(Operator operator, Operand operand) {
         this.operator = operator;
         this.operand = operand;
+    }
+
+
+    public static Operation pop(Deque<Character> deque) {
+        Operator operator = null;
+        Operand operand = null;
+        boolean end = false;
+        while (!end) {
+            char code = deque.pop();
+            Optional<SymbolProvider> optionalResult = Translator.decode(code);
+            if (optionalResult.isEmpty())
+                throw new IllegalArgumentException("코드에 잘못된 문자가 포함 되어 있습니다. : " + code);
+            SymbolProvider result = optionalResult.get();
+            if (result instanceof Operator && operator == null)
+                operator = (Operator) result;
+            else if (result instanceof Operand && operand == null)
+                operand = (Operand) result;
+            else throw new IllegalStateException("디코딩 중 예상치 못한 오류가 발생 했습니다. : " + code);
+
+            if (operator != null && operand != null) {
+                end = true;
+            }
+        }
+        return new Operation(operator, operand);
     }
 
     Operator getOperator() {
@@ -33,9 +61,6 @@ class Operations {
     private Operations() {
     }
 
-    static Operations init() {
-        return new Operations();
-    }
 
     void add(Operation operation) {
         operations.add(operation);
@@ -44,4 +69,14 @@ class Operations {
     public ArrayList<Operation> getOperations() {
         return operations;
     }
+
+    public static Operations of(String string) {
+        Operations operations = new Operations();
+        Deque<Character> deque = Translator.dequeOf(string);
+        while (deque.peekFirst() != null) {
+            operations.add(Operation.pop(deque));
+        }
+        return operations;
+    }
+
 }
