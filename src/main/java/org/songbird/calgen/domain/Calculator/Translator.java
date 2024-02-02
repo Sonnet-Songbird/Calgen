4fdcffe4489dda19378e8c98f45c97b491d8a001
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-02-02 16:04:39 +0900

    FEAT: Add method dequeOf(String), decode(char code)[WIP] in Translator

--- src/main/java/org/songbird/calgen/domain/Calculator/Translator.java
+++ src/main/java/org/songbird/calgen/domain/Calculator/Translator.java
@@ -1,12 +1,36 @@
 package org.songbird.calgen.domain.Calculator;
 
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Optional;
+
 class Translator {
 
-    public static Operations decode(String sourceCode) {
-        int pointer = 0;
-        Operations operations = Operations.init();
 
-        return operations;
+    public static Deque<Character> dequeOf(String sourceCode) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < sourceCode.length(); i++) {
+            deque.add(sourceCode.charAt(i));
+        }
+
+        return deque;
+    }
+
+    //Todo: (추후 개선사항) 가능하면 getBySymbol 부분의 메소드를 분리하고 싶음.
+    public static Optional<SymbolProvider> decode(char code) {
+        Optional<SymbolProvider> result;
+        result = Operator.getBySymbol(code);
+        if (result.isPresent()) {
+            return result;
+        }
+        result = OperandType.getBySymbol(code);
+        if (result.isPresent())
+            return result;
+
+        result = ClausalSymbol.getBySymbol(code);
+        // ClausalSymbol Solver
+        return result;
     }
 
 
@@ -15,7 +39,7 @@ class Translator {
 todo:코드 체크 정규식이 아니라 포인터와 인터프리터식으로 구현
     operation은 2개 이상의 char 해석을 통해 구성되므로 빌더 클래스 필요.
     무상태성 메소드들만 Translator에 보유시킬 것. 해석 책임은 순수 함수로 여기에 구현
-    상태성을 지니는 Builder 클래스를 만들어 Translator 클래스를 호출해서 작동하도록 할 것. (in String to Operations)
+    상태성을 지니는 Builder 클래스를 만들어 Translator 클래스를 호출해서 작동하도록 할 것. (in String
 
 todo:SubCalculrator 구현,
     SubCalculrator 인수처리 구현,
