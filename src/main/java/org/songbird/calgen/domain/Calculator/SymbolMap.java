73830ff5dbe6ec9058339feb703da6c46b630757
Sonnet_Songbird <sonnet4235@gmail.com>
2024-02-04 01:16:27 +0900

    DOCS: Add Todo

--- src/main/java/org/songbird/calgen/domain/Calculator/SymbolMap.java
+++ src/main/java/org/songbird/calgen/domain/Calculator/SymbolMap.java
@@ -1,3 +1,6 @@
+//todo: mutable static 상수 Map<Charactor, SymbolProviider>를 만들어서, 각 클래스의 static 블록에서 여기에 regist하고,
+//      가능한 클래스의 타입을 알아내지 않고 객체의 다형성만을 이용해서 작동하도록 해보자.
+//      해당 클래스에 종속된 동작은 모두 해당 클래스의 책임으로.
 //package org.songbird.calgen.domain.Calculator;
 //
 //import java.util.HashMap;
