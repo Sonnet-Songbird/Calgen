f21baa60242afa20e529b4598ac4cc775baa50fa
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-02-07 12:47:39 +0900

    DOCS: Move implementation memo to note.md from Translator.java

--- docs/note.md
+++ docs/note.md
@@ -9,3 +9,16 @@
 - Calculator에 input을 요구하는 operand가 등록되면, 타게팅이 가능하도록 id로 식별하는 input 수신자 목록을 생성해 관리
 - 체크 여부에 따라 on off할 수 있는 연산도 있으면 좋겠다.
 - Input도 입력 받을 때 Calculator로 한번 처리해서 받으면 입력하기 편한가?
+
+
+##### Translator.java
+- Clausal 구현,
+  - SubCalculrator 인수처리 구현,
+  - Deque와 StringTokenizer 기반의 인터프렛
+  - 삼항연산자 구현,
+  - 세미콜론 구분자 없이 정규식으로 구현,
+  - 인수 클래스화,
+
+- SymbolProvider, SymbolMap 부분은 꼭 코드리뷰 받아서 개선안을 듣자
+  - A : 데이터 플로우의 관리는 객체지향적이지만 SymbolProvider의 구현체를 파싱하는 부분은 여전히 다형성이 적용되지 않았다.
+  - -> 인터페이스에 onRegister() 메소드 추가하고 첫 글자에서 enum을 찾는 것 말고는 전부 onRgister() 메소드에 맡길 것 
--- src/main/java/org/songbird/calgen/domain/Calculator/Translator.java
+++ src/main/java/org/songbird/calgen/domain/Calculator/Translator.java
@@ -32,25 +32,6 @@ class Translator {
 //        // ClausalSymbol Solver
         return result;
     }
-
-
-
-/*
-todo:코드 체크 정규식이 아니라 포인터와 인터프리터식으로 구현
-    operation은 2개 이상의 char 해석을 통해 구성되므로 빌더 클래스 필요.
-    무상태성 메소드들만 Translator에 보유시킬 것. 해석 책임은 순수 함수로 여기에 구현
-    상태성을 지니는 Builder 클래스를 만들어 Translator 클래스를 호출해서 작동하도록 할 것. (in String
-
-todo:SubCalculrator 구현,
-    SubCalculrator 인수처리 구현,
-    Deque와 StringTokenizer 기반의 인터프렛
-    삼항연산자 구현,
-    세미콜론 구분자 없이 정규식으로 구현,
-    인수 클래스화,
-
-todo:SymbolProvider, SymbolMap 부분은 꼭 코드리뷰 받아서 개선안을 듣자
-    A : 데이터 플로우의 관리는 객체지향적이지만 SymbolProvider의 구현체를 파싱하는 부분은 여전히 다형성이 적용되지 않았다.
- */
 }
 
 
