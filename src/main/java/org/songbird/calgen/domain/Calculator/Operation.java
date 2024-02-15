7d5fea58606bef6414e59f2c0be1d6d037030a66
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-02-15 14:16:17 +0900

    REFACTOR: Change Operation.pop() for Improving the code structure

--- src/main/java/org/songbird/calgen/domain/Calculator/Operation.java
+++ src/main/java/org/songbird/calgen/domain/Calculator/Operation.java
@@ -2,7 +2,6 @@ package org.songbird.calgen.domain.Calculator;
 
 import java.util.ArrayList;
 import java.util.Deque;
-import java.util.Optional;
 
 class Operation {
     private final Operator operator;
@@ -14,28 +13,30 @@ class Operation {
         this.operand = operand;
     }
 
-//todo: 예외 핸들링 클래스를 분리하고 만들 떄, 각 예외 케이스에 따른 동작을 SymbolProvider 구현체에서 처리하여 호출하도록
+    //todo: 예외 핸들링 클래스를 분리하고 만들 떄, 각 예외 케이스에 따른 동작을 SymbolProvider 구현체에서 처리하여 호출하도록
     public static Operation pop(Deque<Character> deque) {
         Operator operator = null;
         Operand operand = null;
-        boolean end = false;
-        while (!end) {
+
+        while (!deque.isEmpty()) {
             char code = deque.pop();
-            Optional<Symbolic> optionalResult = Translator.decode(code);
-            if (optionalResult.isEmpty())
-                throw new IllegalArgumentException("코드에 잘못된 문자가 포함 되어 있습니다. : " + code);
-            Symbolic result = optionalResult.get();
-            if (result instanceof Operator && operator == null)
+            Symbolic result = Translator.decode(code)
+                    .orElseThrow(() -> new IllegalArgumentException("코드에 잘못된 문자가 포함 되어 있습니다. : " + code));
+
+            if (result instanceof Operator && operator == null) {
                 operator = (Operator) result;
-            else if (result instanceof Operand && operand == null)
+            } else if (result instanceof Operand && operand == null) {
                 operand = (Operand) result;
-            else throw new IllegalStateException("디코딩 중 예상치 못한 오류가 발생 했습니다. : " + code);
+            } else {
+                throw new IllegalStateException("디코딩 중 예상치 못한 오류가 발생 했습니다. : " + code);
+            }
 
             if (operator != null && operand != null) {
-                end = true;
+                return new Operation(operator, operand);
             }
         }
-        return new Operation(operator, operand);
+
+        throw new IllegalArgumentException("덱이 비었거나 유효한 연산을 찾을 수 없습니다.");
     }
 
     Operator getOperator() {
@@ -49,6 +50,7 @@ class Operation {
     public void setOperand(Constant constant) {
         this.operand = constant;
     }
+
     public Subject operate(Subject subject) {
         return operator.apply(subject, operand);
     }
